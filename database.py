7193831ac13f258d389ee4355219bff51fc46b96
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-08-02 13:47:59 -0300

    Create initial querys

--- database.py
+++ database.py
@@ -1,7 +1,35 @@
 import sqlite3
 
-
 connection = sqlite3.connect("data.db")
+
+# title, author, year, isbn, number of pages, edition, publisher, language, genre, description, image, readed
+CREATE_BOOKS_TABLE = """CREATE TABLE IF NOT EXISTS books (
+    title TEXT,
+    author TEXT,
+    year INTEGER,
+    isbn TEXT,
+    pages INTEGER,
+    edition TEXT,
+    publisher TEXT,
+    language TEXT,
+    genre TEXT,
+    description TEXT,
+    image TEXT,
+    readed INTEGER
+);
+"""
+INSERT_BOOKS = """INSERT INTO books (title, author, year, isbn, pages, edition, publisher, language, genre, description, image, readed) 
+                VALUES (?, ?, 0, ?, 0, ?, ?, ?, ?, ?, ?, 0);"""
+
+SELECT_ALL_BOOKS = "SELECT * FROM books;"
+SELECT_BOOK_BY_TITLE = "SELECT * FROM books WHERE title = ?;"
+SELECT_BOOK_BY_AUTHOR = "SELECT * FROM books WHERE author = ?;"
+SELECT_BOOK_BY_YEAR = "SELECT * FROM books WHERE year = ?;"
+SELECT_BOOK_BY_ISBN = "SELECT * FROM books WHERE isbn = ?;"
+SELECT_BOOK_BY_PUBLISHER = "SELECT * FROM books WHERE publisher = ?;"
+SELECT_BOOK_BY_LANGUAGE = "SELECT * FROM books WHERE language = ?;"
+SELECT_BOOK_BY_GENRE = "SELECT * FROM books WHERE genre = ?;"
+SELECT_READED_BOOKS = "SELECT * FROM books WHERE readed = ?;"
 
 
 def create_tables():
