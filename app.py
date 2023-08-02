78a3373f3030e71460a2eac20caf6a299fc7b932
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-08-02 14:15:23 -0300

    Added 'Add new book' option

--- app.py
+++ app.py
@@ -11,12 +11,28 @@ Your selection: """
 welcome = "Welcome to the pyBookshelf!"
 
 
+def prompt_add_book():
+    title = input("Enter book title: ")
+    author = input("Enter book author: ")
+    year = int(input("Enter book year: "))
+    isbn = input("Enter book ISBN: ")
+    pages = int(input("Enter book number of pages: "))
+    edition = input("Enter book edition: ")
+    publisher = input("Enter book publisher: ")
+    language = input("Enter book language: ")
+    genre = input("Enter book genre: ")
+    description = input("Enter book description: ")
+    image = input("Enter book image: ")
+
+    database.add_book(title, author, year, isbn, pages, edition, publisher, language, genre, description, image)
+
+
 print(welcome)
 database.create_tables()
 
 while (user_input := input(menu)) != "5":
     if user_input == "1":
-        pass
+        prompt_add_book()
     elif user_input == "2":
         pass
     elif user_input == "3":
